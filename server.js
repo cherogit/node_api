de28981b497127641303d8d2323106ba19a7e03b
cherogit <a.chernenko@runway-agency.ru>
2021-01-04 16:20:18 +0300

    added logout route

--- server.js
+++ server.js
@@ -1,287 +1,310 @@
-Error.stackTraceLimit = Infinity
+Error.stackTraceLimit = Infinity;
 
-const Koa = require('koa')
-const Router = require('koa-router')
-const serve = require('koa-static')
-const views = require('koa-views')
-const path = require('path')
-const MongoClient = require('mongodb').MongoClient
-const ObjectId = require('mongodb').ObjectID
-const bodyParser = require('koa-bodyparser')
-const multer = require('koa-multer')
-const dbConfig = require('./config/db')
-let db = null
+const Koa = require("koa");
+const Router = require("koa-router");
+const serve = require("koa-static");
+const views = require("koa-views");
+const path = require("path");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+const bodyParser = require("koa-bodyparser");
+const multer = require("koa-multer");
+const dbConfig = require("./config/db");
+let db = null;
 
 ///////////////////////////////////////////////////////////////// TODO
-const COOKIE_NAME = "session_id"
+const COOKIE_NAME = "session_id";
 const PWD_SALT =
-    "ksyugfkzsgfkdsyg fk`ysg fkyGfkygKFSG KJYS KY WKEUR EKRYGEKF TKESGYDM DS"
+  "ksyugfkzsgfkdsyg fk`ysg fkyGfkygKFSG KJYS KY WKEUR EKRYGEKF TKESGYDM DS";
 
-const crypto = require("crypto")
-const util = require("util")
-const pbkdf2 = util.promisify(crypto.pbkdf2)
+const crypto = require("crypto");
+const util = require("util");
+const pbkdf2 = util.promisify(crypto.pbkdf2);
 const hashPassword = async (password) => {
-    return (await pbkdf2(password, PWD_SALT, 100000, 64, "sha512")).toString(
-        "hex"
-    )
-}
+  return (await pbkdf2(password, PWD_SALT, 100000, 64, "sha512")).toString(
+    "hex"
+  );
+};
 /////////////////////////////////////////////////////////////////
 
-const validators = require('./schemes')
+const validators = require("./schemes");
 
 const checkers = {
-    objectIdIsValid(id) {
-        return ObjectId.isValid(id)
-    }
-}
+  objectIdIsValid(id) {
+    return ObjectId.isValid(id);
+  },
+};
 
-MongoClient.connect(dbConfig.url, {
-    useUnifiedTopology: true
-}, (err, database) => {
+MongoClient.connect(
+  dbConfig.url,
+  {
+    useUnifiedTopology: true,
+  },
+  (err, database) => {
     if (err) {
-        return console.log(err)
+      return console.log(err);
     } else {
-        console.log(`connected to DB {koajs}`)
-        db = database.db(dbConfig.dbName)
+      console.log(`connected to DB ${dbConfig.dbName}`);
+      db = database.db(dbConfig.dbName);
     }
-})
+  }
+);
 
-const app = new Koa()
-const router = new Router()
-const upload = multer()
+const app = new Koa();
+const router = new Router();
+const upload = multer();
 
-app.use(bodyParser())
+app.use(bodyParser());
 
-app.use(serve(path.join(__dirname, '/public')))
-app.use(views(path.join(__dirname, '/app/views'), {extension: 'pug'}))
+app.use(serve(path.join(__dirname, "/public")));
+app.use(views(path.join(__dirname, "/app/views"), { extension: "pug" }));
 
 app.use(async (ctx, next) => {
-    try {
-        await next()
-        const status = ctx.status || 404
-        if (status === 404) {
-            ctx.throw(404)
-        }
-    } catch (err) {
-        if (err.errors) {
-            console.error("validation errors:", err.errors)
-        } else {
-            console.error(err)
-        }
-
-        ctx.status = err.status || 500
-
-        await ctx.render('error', {
-            title: `Ошибка ${ctx.status}`,
-            message: err.message
-        })
+  try {
+    await next();
+    const status = ctx.status || 404;
+    if (status === 404) {
+      ctx.throw(404);
     }
-})
-
-app.on('error', (err, ctx) => {
-    console.error('server error', err)
-})
-
-router.get('/registration', async ctx => {
-
-    await ctx.render('registration', {
-        title: 'Registration',
-        message: 'welcome'
-    })
-})
-
-router.post('/registration', async ctx => {
-    const {login, userName, password} = ctx.request.body
-
-    const existingLogin = await db.collection(`users`).findOne({login})
-
-    if (existingLogin) {
-        ctx.throw(409, `User с таким login уже существует.`)
+  } catch (err) {
+    if (err.errors) {
+      console.error("validation errors:", err.errors);
+    } else {
+      console.error(err);
     }
 
-    const hashedPassword = await hashPassword(password)
+    ctx.status = err.status || 500;
 
-    await db.collection(`users`).insertOne({login, userName, hashedPassword})
+    await ctx.render("error", {
+      title: `Ошибка ${ctx.status}`,
+      message: err.message,
+    });
+  }
+});
 
-    await ctx.render('registration', {
-        title: 'Registration',
-        message: `Welcome, ${userName}`
-    })
-})
+app.on("error", (err, ctx) => {
+  console.error("server error", err);
+});
 
-router.get('/auth', async ctx => {
+router.get("/registration", async (ctx) => {
+  await ctx.render("registration", {
+    title: "Registration",
+    message: "welcome",
+  });
+});
 
-    await ctx.render('auth', {
-        title: 'Authorization',
-        message: 'Sign in'
-    })
-})
+router.post("/registration", async (ctx) => {
+  const { login, userName, password } = ctx.request.body;
+
+  const existingLogin = await db.collection(`users`).findOne({ login });
+
+  if (existingLogin) {
+    ctx.throw(409, `User с таким login уже существует.`);
+  }
+
+  const hashedPassword = await hashPassword(password);
+
+  await db.collection(`users`).insertOne({ login, userName, hashedPassword, cookies: [] });
+
+  await ctx.render("registration", {
+    title: "Registration",
+    message: `Welcome, ${userName}`,
+  });
+});
+
+router.get("/auth", async (ctx) => {
+  await ctx.render("auth", {
+    title: "Authorization",
+    message: "Sign in",
+  });
+});
 
 app.use(async (ctx, next) => {
-    const cookieValue = ctx.cookies.get(COOKIE_NAME)
+  const cookieValue = ctx.cookies.get(COOKIE_NAME);
 
-    ctx.user = await db.collection("users").findOne(
-        {
-            "cookies.name": COOKIE_NAME,
-            "cookies.value": cookieValue,
-        },
-        {
-            projection: {
-                passwordHash: 0,
-                cookies: 0,
-            },
-        }
-    )
-
-    if (!["/auth"].includes(ctx.path)) {
-        if (!ctx.user) ctx.throw(403, "You are not logged in, please log in")
+  ctx.user = await db.collection("users").findOne(
+    {
+      "cookies.name": COOKIE_NAME,
+      "cookies.value": cookieValue,
+    },
+    {
+      projection: {
+        hashedPassword: 0,
+        cookies: 0,
+      },
     }
+  );
 
-    await next()
-})
+  if (!["/auth", '/registration'].includes(ctx.path)) {
+    if (!ctx.user) ctx.throw(403, "You are not logged in, please log in");
+  }
 
-router.post('/auth', async ctx => {
-    const {login, password} = ctx.request.body
+  await next();
+});
 
-    const user = await db.collection(`users`).findOne({login: login})
-    if (!user) ctx.throw(403, "User is not defined")
+router.post("/auth", async (ctx) => {
+  const { login, password } = ctx.request.body;
 
-    const hashedPassword = await hashPassword(password)
-    if (hashedPassword !== user.hashedPassword) {
-        ctx.throw(403, "Неверный пароль")
-    }
+  const user = await db.collection(`users`).findOne({ login: login });
+  if (!user) ctx.throw(403, "User is not defined");
 
-    const cookie = {
-        name: COOKIE_NAME,
-        value: Math.random().toString().slice(2),
-        expires: new Date(Date.now() + 1000 * 86400 * 365),
-    }
+  const hashedPassword = await hashPassword(password);
+  if (hashedPassword !== user.hashedPassword) {
+    ctx.throw(403, "Неверный пароль");
+  }
 
-    await db.collection(`users`).updateOne({login: login}, {$set: {cookies: cookie}})
+  const cookie = {
+    name: COOKIE_NAME,
+    value: Math.random().toString().slice(2),
+    expires: new Date(Date.now() + 1000 * 86400 * 365),
+  };
 
-    ctx.cookies.set(cookie.name, cookie.value, {
-        expires: cookie.expires,
-        httpOnly: true,
-    })
+  await db
+    .collection(`users`)
+    .updateOne({ login: login }, { $addToSet: { cookies: cookie } });
 
-    ctx.redirect('/')
-})
+  ctx.cookies.set(cookie.name, cookie.value, {
+    expires: cookie.expires,
+    httpOnly: true,
+  });
 
-router.get('/', async ctx => {
+  ctx.redirect("/");
+});
 
-    await ctx.render('index', {
-        title: 'Home',
-        isIndex: true,
-        message: 'Home Page'
-    })
-})
+router.get("/logout", async (ctx) => {
+  if (!ctx.user) return ctx.redirect("/");
 
-router.get('/notes', async ctx => {
+  const cookieValue = ctx.cookies.get(COOKIE_NAME);
 
-    const dbNotes = await db.collection(`test`).find({}).toArray()
+  console.log(ctx.user);
 
-    await ctx.render('notes', {
-        title: 'notes list',
-        isNotes: true,
-        message: 'Notes Page',
-        notes: dbNotes
-    })
-})
+  await db
+    .collection("users")
+    .updateOne(
+      { _id: ObjectId(ctx.user._id) },
+      { $pull: { cookies: { $elemMatch: { value: cookieValue } } } }
+    );
 
-router.get('/note', async ctx => {
-    await ctx.render('create-note', {
-        title: 'create-note',
-        isCreate: true,
-        message: 'Create Note Page'
-    })
-})
+    ctx.cookies.set(COOKIE_NAME)
 
-router.get('/note/:id', async ctx => {
-    const id = ctx.params.id
+  ctx.redirect('/')
+});
 
-    if (!checkers.objectIdIsValid(id)) ctx.throw(400, `id is not valid`)
+router.get("/", async (ctx) => {
+  await ctx.render("index", {
+    title: "Home",
+    isIndex: true,
+    message: "Home Page",
+  });
+});
 
-    const note = await db.collection(`test`).findOne({'_id': ObjectId(id)})
+router.get("/notes", async (ctx) => {
+  const dbNotes = await db.collection(`test`).find({}).toArray();
 
-    if (note) {
-        await ctx.render('note', {
-            title: 'note',
-            isNote: true,
-            message: `Note is "${note.note}"`,
-            note: note
-        })
-    } else {
-        // let err = new Error('Page Not Found')
-        // err.statusCode = 404
-        // err.message = `note with id ${id} is not found`
-        // throw err
+  await ctx.render("notes", {
+    title: "notes list",
+    isNotes: true,
+    message: "Notes Page",
+    notes: dbNotes,
+  });
+});
 
-        ctx.throw(400, `note with id ${id} is not found`)
-    }
-})
+router.get("/note", async (ctx) => {
+  await ctx.render("create-note", {
+    title: "create-note",
+    isCreate: true,
+    message: "Create Note Page",
+  });
+});
 
-router.get('/update/:id', async ctx => {
-    const id = ctx.params.id
+router.get("/note/:id", async (ctx) => {
+  const id = ctx.params.id;
 
-    if (!checkers.objectIdIsValid(id)) ctx.throw(400, `id is not valid`)
+  if (!checkers.objectIdIsValid(id)) ctx.throw(400, `id is not valid`);
 
-    const note = await db.collection(`test`).findOne({'_id': ObjectId(id)})
+  const note = await db.collection(`test`).findOne({ _id: ObjectId(id) });
 
-    if (note) {
-        await ctx.render('update-note', {
-            title: 'update-note',
-            message: `update Note "${note.note}"`,
-            note: note
-        })
-    } else {
-        ctx.throw(404, `note with id ${id} is not found`)
-    }
-})
+  if (note) {
+    await ctx.render("note", {
+      title: "note",
+      isNote: true,
+      message: `Note is "${note.note}"`,
+      note: note,
+    });
+  } else {
+    // let err = new Error('Page Not Found')
+    // err.statusCode = 404
+    // err.message = `note with id ${id} is not found`
+    // throw err
 
-router.post('/note', async ctx => {
-    const resultValidation = await validators.noteValidator(ctx.request.body)
+    ctx.throw(400, `note with id ${id} is not found`);
+  }
+});
 
-    if (!resultValidation) console.error(validators.noteValidator.errors)
+router.get("/update/:id", async (ctx) => {
+  const id = ctx.params.id;
 
-    const {title, note} = ctx.request.body
-    const existingNote = await db.collection(`test`).findOne({title: title})
+  if (!checkers.objectIdIsValid(id)) ctx.throw(400, `id is not valid`);
 
-    if (existingNote) {
-        ctx.throw(409, `заметка с таким заголовком уже существует.`)
-    }
+  const note = await db.collection(`test`).findOne({ _id: ObjectId(id) });
 
-    const result = await db.collection(`test`).insertOne(ctx.request.body)
+  if (note) {
+    await ctx.render("update-note", {
+      title: "update-note",
+      message: `update Note "${note.note}"`,
+      note: note,
+    });
+  } else {
+    ctx.throw(404, `note with id ${id} is not found`);
+  }
+});
 
-    await ctx.render('note', {
-        title: 'note',
-        isCreate: true,
-        message: 'Вы создали заметку, поздравляем!',
-        note: result.ops[0]
-    })
-})
+router.post("/note", async (ctx) => {
+  const resultValidation = await validators.noteValidator(ctx.request.body);
 
-router.put('/update/:id', upload.none(), async ctx => {
-    await validators.noteValidator(ctx.request.body)
+  if (!resultValidation) console.error(validators.noteValidator.errors);
 
-    const id = ctx.req.body.id
-    const title = ctx.req.body.title
-    const note = ctx.req.body.note
+  const { title, note } = ctx.request.body;
+  const existingNote = await db.collection(`test`).findOne({ title: title });
 
-    await db.collection(`test`).updateOne({'_id': ObjectId(id)}, {$set: {title: title, note: note}})
-})
+  if (existingNote) {
+    ctx.throw(409, `заметка с таким заголовком уже существует.`);
+  }
 
-router.delete('/note/:id', async ctx => {
-    const id = ctx.params.id
+  const result = await db.collection(`test`).insertOne(ctx.request.body);
 
-    if (!checkers.objectIdIsValid(id)) ctx.throw(400, `id is not valid`)
+  await ctx.render("note", {
+    title: "note",
+    isCreate: true,
+    message: "Вы создали заметку, поздравляем!",
+    note: result.ops[0],
+  });
+});
 
-    const note = await db.collection(`test`).findOne({id: id})
+router.put("/update/:id", upload.none(), async (ctx) => {
+  await validators.noteValidator(ctx.request.body);
 
-    await db.collection(`test`).deleteOne({'_id': ObjectId(id)})
-})
+  const id = ctx.req.body.id;
+  const title = ctx.req.body.title;
+  const note = ctx.req.body.note;
 
-app.use(router.routes())
+  await db
+    .collection(`test`)
+    .updateOne({ _id: ObjectId(id) }, { $set: { title: title, note: note } });
+});
+
+router.delete("/note/:id", async (ctx) => {
+  const id = ctx.params.id;
+
+  if (!checkers.objectIdIsValid(id)) ctx.throw(400, `id is not valid`);
+
+  const note = await db.collection(`test`).findOne({ id: id });
+
+  await db.collection(`test`).deleteOne({ _id: ObjectId(id) });
+});
+
+app.use(router.routes());
 
 app.listen(8000, `0.0.0.0`, () => {
-    console.log('Listen with Koa ' + 8000)
-})
+  console.log("Listen with Koa " + 8000);
+});
