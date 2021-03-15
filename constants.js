bcc0b8022ad6c8acfe45c50fe1cbc42122c80882
cherogit <a.chernenko@runway-agency.ru>
2021-03-15 16:23:42 +0300

    modifier userRoles, refactor constants and create config

--- constants.js
+++ constants.js
@@ -1,5 +1,3 @@
-exports.COOKIE_NAME = 'session_id'
-exports.PWD_SALT = 'ksyugfkzsgfkdsyg fk`ysg fkyGfkygKFSG KJYS KY WKEUR EKRYGEKF TKESGYDM DS'
 exports.PERMISSIONS = {
     createNote: 'createNote',
     updateNote: 'updateNote',
