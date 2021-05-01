65368c7edc36c9c6faa0d00b0b6bb91557f84fef
cherogit <a.chernenko@runway-agency.ru>
2021-05-01 14:42:12 +0300

    fix usage variables from config.js

--- db/index.js
+++ db/index.js
@@ -1,6 +1,4 @@
-const DB_HOST = process.env.DB_HOST
-const DB_PORT = process.env.DB_PORT
-const DB_NAME = process.env.DB_NAME
+const {DB_HOST, DB_PORT, DB_NAME} = require('../config/config')
 
 const dbConfig = {
     url: `mongodb://${DB_HOST}:${DB_PORT}`,
